b9dc91b13b4b21a8017a83e66269c277632f6aca
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-16 10:13:04 +0000

    authenticating users

--- auth.ts
+++ auth.ts
@@ -4,7 +4,8 @@ import Credentials from 'next-auth/providers/credentials';
 import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
- 
+import { z } from 'zod';
+
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
@@ -13,8 +14,27 @@ async function getUser(email: string): Promise<User | undefined> {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
   }
-} 
+}
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [Credentials({})],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+        }
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
 });
